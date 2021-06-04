20c6681e7f5722044ee8cb0b210ef6154c7fa5bb
Malcak <malcakx@gmail.com>
2021-06-04 04:44:01 -0500

    fixed item title

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -5,7 +5,7 @@ export const GifGridItem = ({ id, title, url }) => {
   return (
     <div className="card animate__animated animate__fadeIn animate__delay-2s">
       <img src={url} alt={title} />
-      <p>title</p>
+      <p>{title}</p>
     </div>
   );
 };
