e8eb3b8acc87bb6dbab48d9ad14d7d6687237be6
Malcak <malcakx@gmail.com>
2021-06-04 04:37:17 -0500

    deleted unnecessary component reboot for each test

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -10,10 +10,7 @@ describe('tests on the <GifGridItem /> component', () => {
     url: '"https://media2.giphy.com/media/VXJWhaO7afRe/giphy.gif?cid=14cc3d561t7fzi4lu9a0uzxjbo3z5g7di6uhuhzc9go1k0b6&rid=giphy.gif&ct=g"',
   };
 
-  let wrapper = shallow(<GifGridItem {...item} />);
-  beforeEach(() => {
-    wrapper = shallow(<GifGridItem {...item} />);
-  });
+  const wrapper = shallow(<GifGridItem {...item} />);
 
   test('should display the <GifGridItem /> component correctly', () => {
     expect(wrapper).toMatchSnapshot();
